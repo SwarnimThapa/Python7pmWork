0b80e79d6a9d2cc075d533bc8a9bd617166df5ed
SwarnimThapa <87191433+SwarnimThapa@users.noreply.github.com>
2021-07-22 23:16:24 +0545

    Add files via upload

---
+++ Array.py
@@ -0,0 +1,16 @@
+user = [
+    ['ram', 'sita', 'hari', 'gopal', 'sophia'],
+    ['binita', 'sunita', 'ram', 'madan', 'abc'],
+    ['binita', 'sunita', 'ram','madan','abc']
+]
+x = 0
+while x<len(user):
+
+    y = 0
+    while y<5:
+        print(user[x][y])
+        y+=1
+
+
+    x+=1
+
---
+++ Table.py
@@ -0,0 +1,15 @@
+# table = int(input("Enter table of."))
+# tableupto = int(input("Table upto?"))
+#
+# x = 1
+# while x <= tableupto:
+#     mult = table*x
+#     print(table ," x " ,x ," = " ,mult)
+#     x+=1
+#
+#
+
+
+
+
+
